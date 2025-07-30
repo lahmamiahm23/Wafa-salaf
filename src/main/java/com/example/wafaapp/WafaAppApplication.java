65479978a8957f1a9efaf1b85a138345ad8ce08d
lahmamiahm23 <lahmamiahmed23@gmail.com>
2025-07-30 18:05:06 +0100

    premier commit

---
+++ src/main/java/com/example/wafaapp/WafaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wafaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WafaAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/wafaapp/entities/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.wafaapp.entities;
+
+public enum UserRole {
+        MANAGER, ADMIN, USER
+    }
